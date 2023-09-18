87abea944d91ac63c2b618104db5798b1deb3e11
David Kisari <kisari.david@gmail.com>
2023-09-18 10:30:27 +0200

    menu changes

--- src/views/Media.vue
+++ src/views/Media.vue
@@ -1,33 +1,10 @@
 <template>
   <div class="content">
     <heading>
-      <div slot="main">{{ $t('main') }}</div>
+      <div slot="main">Média</div>
+      <div slot="description">Külső Médiamegjelenések</div>
     </heading>
     <container>
-      <div class="sub-heading" style="font-size: 24px;">VLOG</div>
-      <section v-for="(gallery, gIndex) in galleries" :key="gIndex">
-        <div class="sub-heading">{{ gallery.title }}</div>
-        <p>{{ gallery.desc }}</p>
-        <div class="video-gallery">
-          <div v-for="(ep, eIndex) of gallery.eps.slice(0, 3)" :key="eIndex">
-            <div class="vlog-heading">{{ eIndex + 1 }}. rész</div>
-            <YoutubeVideo v-bind:url="ep" />
-          </div>
-        </div>
-        <b-collapse v-bind:id="'gallery-' + gIndex">
-          <div class="video-gallery">
-            <div v-for="(ep, oIndex) of gallery.eps.slice(3)" :key="oIndex">
-              <div class="vlog-heading">{{ oIndex + 4 }}. rész</div>
-              <YoutubeVideo v-bind:url="ep" />
-            </div>
-          </div>
-        </b-collapse>
-        <div v-if="gallery.eps.length > 3" class="more-button link" v-b-toggle="'gallery-' + gIndex" variant="primary">
-          <span class="when-open">Kevesebb Rész...</span>
-          <span class="when-closed">További Részek...</span>
-        </div>
-      </section>
-      <div class="sub-heading" style="font-size: 24px;">Külső Médiamegjelenések</div>
       <p v-for="(link, index) of mediaAppearances" :key="index">
         <a :href="link.url">{{ link.name }}</a>
       </p>
@@ -36,30 +13,6 @@
 </template>
 
 <style lang="scss" scoped>
-section>div {
-  margin-bottom: 0;
-}
-
-.vlog-heading {
-  font-weight: bold;
-  margin-block: 8px;
-}
-
-.more-button {
-  text-align: right;
-}
-
-.collapsed > .when-open,
-.not-collapsed > .when-closed {
-  display: none;
-}
-@media (min-width: 768px) {
-  .video-gallery {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: 20px;
-  }
-}
 </style>
 
 <script lang="ts">
@@ -73,58 +26,6 @@ import YoutubeVideo from '@/components/moyo/youtube-video.vue';
   components: { container, heading, YoutubeVideo },
 })
 export default class Media extends Vue {
-  galleries = [
-    {
-      title: '2023. tavasz, Uganda',
-      desc: 'És ismét adományok begyűjtve, csomagok készen, hiszen Panna, Juli és Zsófi Uganda felé vették az irányt, hogy a jelképes örökbefogadó szülők ajándékait és kedves üzeneteit továbbítsák az általuk támogatott gyerekeknek és összegyűjtsék az aktuális híreket a manafwai suli lurkóiról.',
-      eps: [
-        'https://www.youtube-nocookie.com/embed/eiIDIQg1BmU',
-      ]
-    },
-    {
-      title: '2023. január, Kenya',
-      desc: 'Zsófi, Balázs és Dr. Balázs januárban hosszas előkészítéseket követően útnak indultak az első önálló, nagyobb ívű kenyai missziónkra, amely során az idő első felében Mombassában és a tengerparti térségben voltak, majd pedig Nairobi nyomornegyedei felé vették az irányt.',
-      eps: [
-        'https://www.youtube-nocookie.com/embed/4xD-M7rfHUw',
-        'https://www.youtube-nocookie.com/embed/UPjEr1Z5H4k',
-      ]
-    },
-    {
-      title: '2022. ősz, Uganda és Kenya',
-      desc: 'Zsófi és Dr. Balázs hosszú útra pakoltak, ugyanis egy egy hónapig tartó misszióra utaztak Ugandába. Az eredeti úticél Manafwa volt, de aztán egy pár napos kitérőt tettek Karamojába is, ahol izgalmas új dolgok vették kezdetüket. Végül pedig Zsófi még egy hétre Kenyába is átutazott.',
-      eps: [
-        'https://www.youtube-nocookie.com/embed/j3hXOyV-JWE',
-        'https://www.youtube-nocookie.com/embed/VMrP_GksjXc',
-        'https://www.youtube-nocookie.com/embed/4EF3cgYQYkc',
-        'https://www.youtube-nocookie.com/embed/8PVEAFqv8dc',
-      ]
-    },
-    {
-      title: '2022. tavasz, Uganda',
-      desc: 'Juli, Móni, Zoli és Zsófi Ugandába utaztak, ahol 3 hetet töltöttek Manafwában majd pedig felkerekedtek Uganda – ha nem a világ – egyik legszebb pontjára, a Murchinson Falls Nemzeti Parkba, sőt útközben még egy orrszarvú rezervátumot is meglátogattak.',
-      eps: [
-        'https://www.youtube-nocookie.com/embed/0eSG6uV1YWM',
-        'https://www.youtube-nocookie.com/embed/rQzr0upAi3E',
-        'https://www.youtube-nocookie.com/embed/SKjz8qx0RH0',
-        'https://www.youtube-nocookie.com/embed/l_9sHjbe3YM',
-      ]
-    },
-    {
-      title: '2021. ősz, Uganda',
-      desc: 'Zsófi és Czikkcakk Panni egy hónapra Ugandába utaztak és ebben a vlog sorozatban mutatják meg nektek, hogyan telnek a missziós mindennapjaik Manafwában.',
-      eps: [
-        'https://www.youtube-nocookie.com/embed/-9CmuP4lB88',
-        'https://www.youtube-nocookie.com/embed/2kppsBe8vss',
-        'https://www.youtube-nocookie.com/embed/k-QZu2GQafU',
-        'https://www.youtube-nocookie.com/embed/jeaF2EjEPmQ',
-        'https://www.youtube-nocookie.com/embed/ucEJRKE8cHQ',
-        'https://www.youtube-nocookie.com/embed/PrHcf30Sfic',
-        'https://www.youtube-nocookie.com/embed/p2LVw95V8Pc',
-        'https://www.youtube-nocookie.com/embed/ooAEdnu6CKY',
-      ]
-    },
-  ]
-
   mediaAppearances = [
     { name: 'WMN', url: 'https://wmn.hu/ugy/58214-a-nok-nem-a-bunuktol-tisztulnak-a-menstruacioval--moshato-betet-varrasat-tanitjak-magyarok-afrikaban?fbclid=IwAR3Sr4qmt2EPG9zm76QAR9zVSAtsvdlpiWo_4fPY2XQH2ko1liNC9GY50c4' },
     { name: 'VIP', url: 'https://thevip.hu/2022/01/24/magyarok-kelet-afrikaban-hetente-ketszer-haromszor-ettek-de-boldogok-es-eletvidamak-voltak/' },
@@ -138,14 +39,3 @@ export default class Media extends Vue {
   ]
 }
 </script>
-
-<i18n>
-{
-  "hu": {
-    "main": "Média"
-  },
-  "en": {
-    "main": "Media"
-  }
-}
-</i18n>
