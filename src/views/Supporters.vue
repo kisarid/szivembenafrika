828a90551131574116ced51885af47109281ca1a
David Kisari <kisari.david@gmail.com>
2024-02-18 11:49:54 +0100

    add new supporter

--- src/views/Supporters.vue
+++ src/views/Supporters.vue
@@ -6,7 +6,11 @@
     <container>
       <section>
         <div class="icons">
-          <div class="icon" v-for="(supporter, index) in supporters" :key="index">
+          <div
+            class="icon"
+            v-for="(supporter, index) in supporters"
+            :key="index"
+          >
             <a :href="supporter.link" target="blank" rel="noreferrer noopener">
               <img :src="supporter.logo" alt="" />
             </a>
@@ -48,20 +52,66 @@ import heading from '@/components/moyo/heading.vue'
 })
 export default class Supporters extends Vue {
   supporters = [
-    { link: 'https://hungaryhelps.gov.hu/', logo: require('@/assets/icons/logo-hunhelps.png') },
-    { link: 'https://emilla.me/', logo: require('@/assets/icons/logo-emilla.png') },
-    { link: 'https://poloneked.hu/', logo: require('@/assets/icons/logo-poloneked.png') },
-    { link: 'https://www.digitalpress.hu/', logo: require('@/assets/icons/logo-digitalpress.png') },
-    { link: 'https://hooplasewing.com/', logo: require('@/assets/icons/logo-hoopla.jpg') },
-    { link: 'https://www.purelifefoundation.eu/', logo: require('@/assets/icons/logo-purelife.jpg') },
-    { link: 'https://www.facebook.com/dorinkastudiohungary/', logo: require('@/assets/icons/logo-dorinka.png') },
-    { link: 'https://zsolya.com/', logo: require('@/assets/icons/logo-zsolya.png') },
-    { link: 'https://wanderergraphic.com/', logo: require('@/assets/icons/logo_wanderergraphic.png') },
-    { link: 'https://www.czikkcakkdesign.com/', logo: require('@/assets/icons/logo_panni.png') },
-    { link: 'https://www.szelencecafe.hu/', logo: require('@/assets/icons/logo_szelence.png') },
-    { link: 'https://zugmuhely.com/', logo: require('@/assets/icons/logo-zug.png') },
-    { link: 'https://harrerchocolat.com/', logo: require('@/assets/icons/logo_harrer.png') },
-    { link: 'https://chococard.hu/', logo: require('@/assets/icons/logo_beantobear.jpg') },
+    {
+      link: 'https://hungaryhelps.gov.hu/',
+      logo: require('@/assets/icons/logo-hunhelps.png'),
+    },
+    {
+      link: 'https://emilla.me/',
+      logo: require('@/assets/icons/logo-emilla.png'),
+    },
+    {
+      link: 'https://poloneked.hu/',
+      logo: require('@/assets/icons/logo-poloneked.png'),
+    },
+    {
+      link: 'https://www.digitalpress.hu/',
+      logo: require('@/assets/icons/logo-digitalpress.png'),
+    },
+    {
+      link: 'https://hooplasewing.com/',
+      logo: require('@/assets/icons/logo-hoopla.jpg'),
+    },
+    {
+      link: 'https://www.purelifefoundation.eu/',
+      logo: require('@/assets/icons/logo-purelife.jpg'),
+    },
+    {
+      link: 'https://www.facebook.com/dorinkastudiohungary/',
+      logo: require('@/assets/icons/logo-dorinka.png'),
+    },
+    {
+      link: 'https://zsolya.com/',
+      logo: require('@/assets/icons/logo-zsolya.png'),
+    },
+    {
+      link: 'https://wanderergraphic.com/',
+      logo: require('@/assets/icons/logo_wanderergraphic.png'),
+    },
+    {
+      link: 'https://www.czikkcakkdesign.com/',
+      logo: require('@/assets/icons/logo_panni.png'),
+    },
+    {
+      link: 'https://www.szelencecafe.hu/',
+      logo: require('@/assets/icons/logo_szelence.png'),
+    },
+    {
+      link: 'https://zugmuhely.com/',
+      logo: require('@/assets/icons/logo-zug.png'),
+    },
+    {
+      link: 'https://harrerchocolat.com/',
+      logo: require('@/assets/icons/logo_harrer.png'),
+    },
+    {
+      link: 'https://chococard.hu/',
+      logo: require('@/assets/icons/logo_beantobear.jpg'),
+    },
+    {
+      link: 'https://popzrt.hu/',
+      logo: require('@/assets/icons/logo-popmerch.png'),
+    },
   ]
 }
 </script>
